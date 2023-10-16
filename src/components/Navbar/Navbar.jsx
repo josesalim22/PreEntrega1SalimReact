1e33884b638b60c2fe6301af1b55cf8beb5875e8
jose salim <josesalim22@yahoo.com>
2023-10-16 08:20:53 -0300

    primer commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+return (
+     <div>
+          <div>
+
+          </div>
+          <div>
+               
+          </div>
+     </div>
+)
+
+
+};
+
+export default Navbar;
