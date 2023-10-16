276e673f9ddfe1f360e716d1777465cc5ece18c0
jose salim <josesalim22@yahoo.com>
2023-10-16 18:01:16 -0300

    crear navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,19 @@
+import './Navbar.css';
 const Navbar = () => {
-return (
-     <div>
-          <div>
-
+     return (
+          <div  className="navbar-container">
+               <div>
+                    <img src={'./images/logo.png'} alt="logo" height={'150px'} />
+               </div>
+               <div>
+                    <ul className="list-item">
+                         <li><button className='category-button'>Productos</button></li>
+                         <li><button className='category-button'>Nosotros</button></li>
+                         <li><button className='category-button'>Contacto</button></li>
+                    </ul>
+               </div>
           </div>
-          <div>
-               
-          </div>
-     </div>
-)
+     )
 
 
 };
